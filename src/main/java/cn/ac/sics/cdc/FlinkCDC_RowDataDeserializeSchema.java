1f483da5e303164b4f34ca3ea4b8e7d1567e908f
JesseAtSZ <1411643015@qq.com>
2022-03-13 17:40:54 +0800

    feat: add DebeziumDeserializeSchema

--- src/main/java/cn/ac/sics/cdc/FlinkCDC_RowDataDeserializeSchema.java
+++ src/main/java/cn/ac/sics/cdc/FlinkCDC_RowDataDeserializeSchema.java
@@ -3,12 +3,22 @@ package cn.ac.sics.cdc;
 import com.ververica.cdc.connectors.mysql.MySqlSource;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.DebeziumSourceFunction;
-import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import com.ververica.cdc.debezium.table.RowDataDebeziumDeserializeSchema;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.runtime.typeutils.InternalTypeInfo;
+import org.apache.flink.table.types.logical.IntType;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.VarCharType;
 
-public class FlinkCDC {
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.time.ZoneId.systemDefault;
+
+public class FlinkCDC_RowDataDeserializeSchema {
   public static void main(String[] args) throws Exception {
     // 1.获取 FlinK 执行环境
     StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -23,24 +33,35 @@ public class FlinkCDC {
     //    env.setStateBackend(new FsStateBackend("file:\\src\\main\\resources"));
 
     // 2.通过Flink CDC 构建 SourceFunction
-    DebeziumSourceFunction<String> sourceFunction =
-        MySqlSource.<String>builder()
+    List<RowType.RowField> fields = new LinkedList<>();
+    fields.add(new RowType.RowField("column_1", new IntType(false)));
+    fields.add(new RowType.RowField("table", new VarCharType(1000)));
+
+    RowType rowType = new RowType(false, fields);
+    InternalTypeInfo<RowData> typeInfo = InternalTypeInfo.of(rowType);
+
+    RowDataDebeziumDeserializeSchema rowDataDebeziumDeserializeSchema =
+        new RowDataDebeziumDeserializeSchema(
+            rowType, typeInfo, ((rowData, rowKind) -> {}), systemDefault());
+
+    DebeziumSourceFunction<RowData> sourceFunction =
+        MySqlSource.<RowData>builder()
             .hostname("192.168.137.200")
             .port(3306)
             .username("root")
             .password("141164")
             .databaseList("tpcc")
             .tableList("tpcc.source")
-            .deserializer(new StringDebeziumDeserializationSchema())
+            .deserializer(rowDataDebeziumDeserializeSchema)
             .startupOptions(StartupOptions.initial())
             .build();
 
-    DataStreamSource<String> dataStreamSource = env.addSource(sourceFunction);
+    DataStreamSource<RowData> dataStreamSource = env.addSource(sourceFunction);
 
     // 3.数据打印
     dataStreamSource.print();
 
     // 4.启动任务
-    env.execute("FlinkCDC");
+    env.execute("FlinkCDC_RowDataDeserializeSchema");
   }
 }
